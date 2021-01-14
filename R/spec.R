55c25eaaf8d729ed99dcc89573db93ed0bb7875e
Hadley Wickham <h.wickham@gmail.com>
2021-01-14 17:04:45 -0600

    Processing feedback

--- R/spec.R
+++ R/spec.R
@@ -122,11 +122,17 @@ spec_reason <- function(call, signaller) {
 }
 
 spec_package <- function(env, signaller) {
-  env <- topenv(env)
 
+  # Hack for vignettes
+  default <- getOption("lifecycle:::calling_package", NULL)
+  if (!is.null(default)) {
+    return(default)
+  }
+
+  env <- topenv(env)
   if (is_reference(env, global_env())) {
     # Convenient for experimenting interactively
-    return(getOption("lifecycle:::calling_package", "<NA>"))
+    return(NA)
   }
 
   if(is_namespace(env)) {
