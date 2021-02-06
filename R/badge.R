cc64f0fc95ad8848e6c98587f564fb7564ca91b4
Hadley Wickham <h.wickham@gmail.com>
2021-02-06 09:55:53 -0600

    Tweak badge format

--- R/badge.R
+++ R/badge.R
@@ -44,43 +44,19 @@
 #'
 #' @export
 badge <- function(stage) {
-  url <- paste0("https://lifecycle.r-lib.org/articles/stages.html#", stage)
-  img <- lifecycle_img(stage, url)
 
-  sprintf("\\ifelse{html}{%s}{\\strong{%s}}", img, upcase1(stage))
-}
-
-lifecycle_img <- function(stage, url) {
-  file <- sprintf("lifecycle-%s.svg", stage)
-  stage_alt <- upcase1(stage)
-
-  switch(stage,
-
-    experimental = ,
-    maturing = ,
-    stable = ,
-    questioning = ,
-    retired = ,
-    superseded = ,
-    archived =
-      sprintf(
-        "\\out{<a href='%s'><img src='%s' alt='%s lifecycle'></a>}",
-        url,
-        file.path("figures", file),
-        stage_alt
-      )
-   ,
-
-    `soft-deprecated` = ,
-    deprecated = ,
-    defunct =
-      sprintf(
-        "\\figure{%s}{options: alt='%s lifecycle'}",
-        file,
-        stage_alt
-      ),
-
-    abort(sprintf("Unknown lifecycle stage '%s'", stage))
-
+  stage <- arg_match0(stage,
+    c("experimental", "stable", "questioning", "superseded", "deprecated")
   )
+
+  url <- paste0("https://lifecycle.r-lib.org/articles/stages.html#", stage)
+  html <- sprintf(
+    "\\href{%s}{\\figure{%s}{options: alt='[%s]'}}",
+    url,
+    file.path(sprintf("lifecycle-%s.svg", stage)),
+    upcase1(stage)
+  )
+  text <- sprintf("\\strong{[%s]}", upcase1(stage))
+
+  sprintf("\\ifelse{html}{%s}{%s}", html, text)
 }
